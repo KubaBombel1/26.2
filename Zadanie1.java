7c2f57742134123a4bac7e32001e38b74221cb16
KubaBombel1 <79452843+KubaBombel1@users.noreply.github.com>
2021-02-28 22:25:47 +0100

    Update Zadanie1.java

--- Zadanie1.java
+++ Zadanie1.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package zadanie.pkg1;
 
-/**
- *
- * @author Kuba
- */
+
 public class Zadanie1 {
    
     public static void main(String[] args) 
